10bd4ce5608a9cc4a5d82c54ba5da778f48a09da
Alison Alves <alisonsantos2017@outlook.com>
2020-07-10 00:20:51 -0300

    Template Angular parte 2

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Usuario } from "../../modelo/usuario";
+import { Router, ActivatedRoute } from "@angular/router";
+import { state } from "@angular/animations";
 
 @Component({
   selector: "app-login",
@@ -6,11 +9,27 @@ import { Component } from "@angular/core";
   styleUrls: ["./login.component.css"]
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  public email = ""
-  entrar() {
-    alert(this.email);
+  public usuario;
+  public returnUrl: string;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute)
+  {   
+  }
+
+  ngOnInit(): void
+  {
+    this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'];
+    this.usuario = new Usuario();
+  }
+
+  entrar()
+  {
+    if (this.usuario.email == "alison@teste.com" && this.usuario.senha == "1234") {
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate([this.returnUrl]);
+    }
   }
 
 }
